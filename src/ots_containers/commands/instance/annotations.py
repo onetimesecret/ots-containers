82bf08c4d62470a5d558668f404dfb8c617406cb
delano <delano@onetimesecret.com>
2026-02-05 17:56:01 -0800

    Use StrEnum instead of (str, Enum) for InstanceType

--- src/ots_containers/commands/instance/annotations.py
+++ src/ots_containers/commands/instance/annotations.py
@@ -13,13 +13,13 @@ Identifier patterns:
 - Scheduler: numeric or named (main, 1)
 """
 
-from enum import Enum
+from enum import StrEnum
 from typing import Annotated
 
 import cyclopts
 
 
-class InstanceType(str, Enum):
+class InstanceType(StrEnum):
     """Type of container instance."""
 
     WEB = "web"
